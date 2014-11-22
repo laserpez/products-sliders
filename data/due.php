e0e3f065c0d8668e95afcf7f90191c7485ea94d7
ika-grumpy <ika.grumpy@gmail.com>
2014-11-22 09:41:49 +0100

    corretto json

--- data/due.php
+++ data/due.php
@@ -2,70 +2,70 @@
     {
         "type": "prod",
         "id": "1",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "9",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/quattro.php"
   },
     {
         "type": "prod",
         "id": "10",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "2",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "3",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "4",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "5",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "6",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "7",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   },
     {
         "type": "prod",
         "id": "8",
-        "name": "WEBCAM 01",
+        "category": "WEBCAM 01",
         "image": "http://www.istitutopaideia.com/animazione/img/UWCAM000200_03_L.jpg",
         "url": "./data/tre.php"
   }
